b9a6384de19146b27cb381b435dc9f036c8e6906
EarlOld <dimonsuper010@gmail.com>
2017-07-11 18:43:22 +0300

    update(menu, header): fixed styles

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import './header.styl'
+import { Row, Col } from 'antd'
 
-const Header = () => {
-  return (
-    <div id='header'>
-      <h1 className='title'>ETHEREUM NAMES</h1>
-    </div>
-  )
+class Header extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+  render () {
+    return (
+      <div id='header'>
+        <Row >
+          <Col span={2} >
+            <div className='header-logo'>
+              <img src='./dist/media/logo.png' />
+            </div>
+          </Col>
+          <Col span={22} >
+            <h1 className='title'>Ethereum names</h1>
+          </Col>
+        </Row>
+      </div>
+    )
+  }
 }
 
 export default Header
--- app/components/menu/menu.jsx
+++ app/components/menu/menu.jsx
@@ -1,43 +1,107 @@
-import React, { Component} from 'react'
-import { Link } from 'react-router'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Link, browserHistory } from 'react-router'
+import { connect } from 'react-redux'
+
 import './menu.styl'
 
 class Menu extends Component {
   constructor () {
     super()
     this.state = {
-      menu: [
-        {
-          title: 'faq',
-          value: '/faq'
-        },
-        {
-          title: 'graph',
-          value: '/'
-        },
-        {
-          title: 'buy',
-          value: '/buy'
-        },
-        {
-          title: 'sell',
-          value: '/sell'
-        }
-      ]
+      faq: 'faq',
+      grafs: 'grafs',
+      buy: 'buy',
+      sell: 'sell'
     }
+  }
+  hendleStatusFaq () {
+    this.props.onHandleMenuFaq()
+    browserHistory.push('/faq')
+  }
+  hendleStatusGraph () {
+    this.props.onHandleMenuGraph()
+    browserHistory.push('/')
+  }
+  hendleStatusSell () {
+    this.props.onHandleMenuSell()
+    browserHistory.push('/sell')
+  }
+  hendleStatusBuy () {
+    this.props.onHandleMenuBuy()
+    browserHistory.push('/buy')
   }
   render () {
     return (
       <div id='menu'>
-        {this.state.menu.map((el, key) => (
-          <Link to={el.value} activeClassName='active' key={key}>
-            <div className='menu-item'>
-              {el.title}
-            </div>
-          </Link>)
-        )}
+        <Link onClick={() => this.hendleStatusFaq()}>
+          <div className={this.props.menu.faqStatus ? 'menu-item-active' : 'menu-item'}>
+            <img src={this.props.menu.faqStatus ? './dist/media/plus-active.png' : './dist/media/plus.png'} />
+            <div className='text-center'>faq</div>
+          </div>
+        </Link>
+        <Link onClick={() => this.hendleStatusGraph()} >
+          <div className={this.props.menu.graphStatus ? 'menu-item-active' : 'menu-item'}>
+            <img src={this.props.menu.graphStatus ? './dist/media/grafs.png' : './dist/media/grafs-off.png'} />
+            <div className='text'>graph</div>
+          </div>
+        </Link>
+        <Link onClick={() => this.hendleStatusBuy()} >
+          <div className={this.props.menu.buyStatus ? 'menu-item-active' : 'menu-item'}>
+            <img src={this.props.menu.buyStatus ? './dist/media/plus-active.png' : './dist/media/plus.png'} />
+            <div className='text-center'>buy</div>
+          </div>
+        </Link>
+        <Link onClick={() => this.hendleStatusSell()} activeClassName='active' >
+          <div className={this.props.menu.sellStatus ? 'menu-item-active' : 'menu-item'}>
+            <img src={this.props.menu.sellStatus ? './dist/media/plus-active.png' : './dist/media/plus.png'} />
+            <div className='text-center'>sell</div>
+          </div>
+        </Link>
       </div>
     )
   }
 }
-export default Menu
+const mapStateToProps = state => {
+  return {
+    router: state.router,
+    blog: state.blog,
+    menu: state.menu
+  }
+}
+const mapDispatchToProps = dispatch => ({
+  onHandleMenuFaq: () => {
+    dispatch({
+      type: 'HANDLE_MENU_FAQ',
+      payload: ''
+    })
+  },
+  onHandleMenuGraph: () => {
+    dispatch({
+      type: 'HANDLE_MENU_GRAPH',
+      payload: ''
+    })
+  },
+  onHandleMenuBuy: () => {
+    dispatch({
+      type: 'HANDLE_MENU_BUY',
+      payload: ''
+    })
+  },
+  onHandleMenuSell: () => {
+    dispatch({
+      type: 'HANDLE_MENU_SELL',
+      payload: ''
+    })
+  }
+})
+
+Menu.propTypes = {
+  onHandleMenuFaq: PropTypes.func,
+  onHandleMenuGraph: PropTypes.func,
+  onHandleMenuBuy: PropTypes.func,
+  onHandleMenuSell: PropTypes.func,
+  menu: PropTypes.object
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Menu)
