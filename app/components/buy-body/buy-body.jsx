90e7ffdf199f10fbc5f383e09b88fc48c59b163f
EarlOld <dimonsuper010@gmail.com>
2017-07-12 19:47:53 +0300

    fixed styles

--- app/components/buy-body/buy-body.jsx
+++ app/components/buy-body/buy-body.jsx
@@ -21,21 +21,21 @@ class BuyBody extends Component {
               <p className='info-title'>TL;DR: Just send ETH to the Purchase Address of the name you want!</p>
               <ol>
                 <li>
-                Pick a name you like from the list on the main page. For example: rumours.eth
-                <img src='./dist/media/buy-photo.png' />
-              </li>
-              <li>
-                Send the amount of ETH shown under Price to the address given under Purchase Address.
-                In this example, you would send at least .05 ETH to 0x123lqowemqwo4e012i.
-              </li>
-              <li>
-                Verify the transaction went through and that the name belongs to you on <a>Etherscan</a>.
-                <img src='./dist/media/buy-photo2.png' />
-              </li>
-              <li>
-                 Congratulations! Your friends can now easily send you ETH! And if at some point you don't like your name anymore, you can always resell it on here or wait 1 year and claim the at least .01 ETH locked in its deed by releasing the name. Congratulations! Your friends can now easily send you ETH! And if at some point you don't like your name anymore, you can always resell it on here or wait
-                  1 year and claim the at least .01 ETH locked in its deed by releasing the name.
-              </li>
+                  Pick a name you like from the list on the main page. For example: rumours.eth
+                  <img src='./dist/media/buy-photo.png' />
+                </li>
+                <li>
+                  Send the amount of ETH shown under Price to the address given under Purchase Address.
+                  In this example, you would send at least .05 ETH to 0x123lqowemqwo4e012i.
+                </li>
+                <li>
+                  Verify the transaction went through and that the name belongs to you on <a>Etherscan</a>.
+                  <img src='./dist/media/buy-photo2.png' />
+                </li>
+                <li>
+                   Congratulations! Your friends can now easily send you ETH! And if at some point you don't like your name anymore, you can always resell it on here or wait 1 year and claim the at least .01 ETH locked in its deed by releasing the name. Congratulations! Your friends can now easily send you ETH! And if at some point you don't like your name anymore, you can always resell it on here or wait
+                    1 year and claim the at least .01 ETH locked in its deed by releasing the name.
+                </li>
               </ol>
             </div>
           </Col>
