04fa15e8e309310be372909451fee376acb01c39
Grimm <Andrey.Grimm.1998@gmail.com>
2017-07-12 19:12:26 +0300

    create sell

--- app/components/sell/sell.jsx
+++ app/components/sell/sell.jsx
@@ -1,6 +1,7 @@
 import Header from '../header/header.jsx'
 import React, { Component } from 'react'
 import Menu from '../menu/menu.jsx'
+import SellBody from '../sell-body/sell-body.jsx'
 import './sell.styl'
 
 class Sell extends Component {
@@ -9,7 +10,7 @@ class Sell extends Component {
       <div id='buy'>
         <Header />
         <Menu />
-        SELL
+        <SellBody />
       </div>
     )
   }
