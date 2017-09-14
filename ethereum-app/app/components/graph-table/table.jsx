fdf8b6cd0a5c7f41219ec8f13df08b967cefdbc5
Grimm <Andrey.Grimm.1998@gmail.com>
2017-09-14 15:58:32 +0300

    Update locale table

--- ethereum-app/app/components/graph-table/table.jsx
+++ ethereum-app/app/components/graph-table/table.jsx
@@ -14,15 +14,7 @@ class GraphTable extends Component {
         pageSize: 20,
         showQuickJumper: true,
         locale: {
-          items_per_page: '/ page',
-          jump_to: '#',
-          next_3: 'Next 3 Pages',
-          next_5: 'Next 5 Pages',
-          next_page: 'Next Page',
-          page: '',
-          prev_3: 'Previous 3 Pages',
-          prev_5: 'Previous 5 Pages',
-          prev_page: 'Previous Page'
+          jump_to: '#'
         }
       },
       isBuyModalVisible: false,
--- ethereum-app/app/components/home/home.jsx
+++ ethereum-app/app/components/home/home.jsx
@@ -1,13 +1,15 @@
 import Header from '../header/header.jsx'
 import React, { Component } from 'react'
+import { LocaleProvider } from 'antd'
 import GraphTable from '../graph-table/table.jsx'
+import enUS from 'antd/lib/locale-provider/en_US'
 
 class Home extends Component {
   render () {
     return (
       <div>
         <Header />
-        <GraphTable />
+        <LocaleProvider locale={enUS}><GraphTable /></LocaleProvider>
       </div>
     )
   }
