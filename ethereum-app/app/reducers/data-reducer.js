69eb03e8549266eed72ea261f64ee0507540bb98
Grimm <Andrey.Grimm.1998@gmail.com>
2017-09-14 14:28:15 +0300

    add sort domains

--- ethereum-app/app/reducers/data-reducer.js
+++ ethereum-app/app/reducers/data-reducer.js
@@ -16,7 +16,7 @@ let initialDataStore = []
 const dataReducer = (state = initialDataStore, action) => {
   switch (action.type) {
   case 'SET_LIST_DOMAINS':
-    return action.payload.map(item => new Domain(item)).reverse()
+    return action.payload.map(item => new Domain(item)).sort((first, last) => last.price - first.price)
   case 'DEL_DOMAIN':
     break
   }
