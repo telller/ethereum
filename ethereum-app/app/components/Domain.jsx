fce7eb4f0b51939c5cf7f7abaeab0e3d775e887a
Grimm <Andrey.Grimm.1998@gmail.com>
2017-09-19 18:06:12 +0300

    update class Domain

--- ethereum-app/app/components/Domain.jsx
+++ ethereum-app/app/components/Domain.jsx
@@ -6,8 +6,6 @@ class Domain {
     this.wallet = obj.attributes[0] && obj.attributes[0].options[0].toLowerCase()
     this.categories = obj.categories.map(item => item.name.toLowerCase())
     this.description = obj.description
-    this.link = obj.permalink
-    this.img = obj.images[0].src
     this.status = obj.status
   }
 }
