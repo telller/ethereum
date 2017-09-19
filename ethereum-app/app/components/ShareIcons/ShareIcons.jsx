216bf43c2ab540e7e135bb0cde2bd36c83e1a957
Grimm <Andrey.Grimm.1998@gmail.com>
2017-09-19 18:05:15 +0300

    Add sharing

---
+++ ethereum-app/app/components/ShareIcons/ShareIcons.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Button } from 'antd'
+import PropTypes from 'prop-types'
+import './ShareIcons.styl'
+
+class ShareIcons extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      url: ''
+    }
+  }
+  facebook (purl, ptitle) {
+    this.state.url = ('http://www.facebook.com/sharer.php?s=100' + '&title=' + encodeURIComponent(ptitle) + '&u=' + encodeURIComponent(purl))
+    window.open(this.state.url, '', 'toolbar=0,status=0,width=626,height=436')
+  }
+  twitter (purl, ptitle) {
+    this.state.url = 'http://twitter.com/share?' + 'text=' + encodeURIComponent(ptitle) + '&url=' + encodeURIComponent(purl) + '&counturl=' + encodeURIComponent(purl)
+    window.open(this.state.url, '', 'toolbar=0,status=0,width=626,height=436')
+  }
+  googleplus (purl, ptitle) {
+    this.state.url = 'https://plus.google.com/share?' + 'text=' + encodeURIComponent(ptitle) + '&url=' + encodeURIComponent(purl) + '&counturl=' + encodeURIComponent(purl)
+    window.open(this.state.url, '', 'toolbar=0,status=0,width=626,height=436')
+  }
+  email (purl, ptitle) {
+    this.state.url = 'mailto:?' + 'subject=' + 'I wanted you to see this site;' + '&body=' + encodeURIComponent(purl) + 'text=' + encodeURIComponent(ptitle)
+    window.open(this.state.url, '', 'toolbar=0,status=0,width=626,height=436')
+  }
+  render () {
+    return (
+      <div className={'share-icons ' + (this.props.className ? this.props.className : '')}>
+        <Button onClick={() => this.facebook(this.props.url, this.props.title)} className={this.state.showIcons
+              ? 'icon-default icon-facebook icon-visible'
+              : 'icon-default icon-facebook'}>
+          <i className='fa fa-facebook' aria-hidden='true' />
+        </Button>
+        <Button onClick={() => this.twitter(this.props.url, this.props.title)} className={this.state.showIcons
+              ? 'icon-default icon-twitter icon-visible'
+              : 'icon-default icon-twitter'}>
+          <i className='fa fa-twitter' aria-hidden='true' />
+        </Button>
+        <Button onClick={() => this.googleplus(this.props.url, this.props.title)} className={this.state.showIcons
+              ? 'icon-default icon-google-plus icon-visible'
+              : 'icon-default icon-google-plus'}>
+          <i className='fa fa-google-plus' aria-hidden='true' />
+        </Button>
+        <Button onClick={() => this.email(this.props.url, this.props.title)} className={this.state.showIcons
+              ? 'icon-default icon-envelope icon-visible'
+              : 'icon-default icon-envelope'}>
+          <i className='fa fa-envelope' aria-hidden='true' />
+        </Button>
+      </div>
+    )
+  }
+}
+
+ShareIcons.defaultProps = {
+  title: 'Ensdomains - site for buying ENS domains'
+}
+
+ShareIcons.propTypes = {
+  url: PropTypes.string,
+  title: PropTypes.string,
+  className: PropTypes.string
+}
+
+export default ShareIcons
--- ethereum-app/app/components/SingleDomain/SingleDomain.jsx
+++ ethereum-app/app/components/SingleDomain/SingleDomain.jsx
@@ -1,65 +1,53 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { Modal } from 'antd'
 import ReactHtmlParser from 'react-html-parser'
+import ShareIcons from '../ShareIcons/ShareIcons.jsx'
 import './SingleDomain.styl'
 
 class SingleDomain extends Component {
   constructor (props) {
     super(props)
-    this.state = {
-      domain: null
-    }
-  }
-
-  componentWillMount () {
-    if (this.props.data) {
-      this.setState((state, props) => {
-        return {
-          domain: props.data
-        }
-      })
-    } else {
-      this.setState({
-        domain: {
-          id: 0,
-          name: 'test',
-          price: 0,
-          wallet: '0x00000000000000000000000000',
-          categories: ['category'],
-          description: '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt laudantium, nemo neque eaque quia esse quibusdam rerum voluptas! Porro et quidem incidunt omnis praesentium eos dolore explicabo, ducimus consectetur natus.</p>',
-          link: location.href,
-          img: 'https://smaulgld.com/wp-content/uploads/2017/05/Ethereum.jpg',
-          status: 'publish'
-        }
-      })
-    }
+    this.imgDomain = 'https://ensdomains.market/wp-content/uploads/2017/09/Ethereum.png'
   }
 
   render () {
     return (
       <Modal
         className='domain-info-modal'
-        width={400}
+        width={600}
         visible
         title='Domain Info:'
         onCancel={this.props.onCancel}
         footer={false}
       >
         <div className='single-domain'>
-          <img width='100px' src={this.state.domain.img} alt={'Ethereum domain ' + this.state.domain.name} />
-          <h2>{this.state.domain.name}</h2>
-          <p>{this.state.domain.price} ETH</p>
-          <p>{this.state.domain.wallet}</p>
-          <ul>
-            {
-              this.state.domain.categories.map(
-                (item, index) => <li key={index}>{item}</li>
-              )
-            }
-          </ul>
-          {ReactHtmlParser(this.state.domain.description)}
+          <img className='domain-logo' src={this.imgDomain} alt='Ethereum domain' />
+          <h2 className='domain-name'>{this.props.data.name}</h2>
+          <ShareIcons className='domain-share-icons' url={location.href} />
+          {
+            !!Number(this.props.data.price) && <div><b>Price:</b> {this.props.data.price} ETH</div>
+          }
+          <div className='domain-wallet'><b>Wallet:</b> {this.props.data.wallet}</div>
+          {
+            !!this.props.data.categories.length && (
+            <div className='domain-categories'>
+              <b>Categories:</b>
+              <ul className='domain-list-categories'>
+                {
+                  this.props.data.categories.map(
+                    (item, index) => <li key={index}>{item}</li>
+                  )
+                }
+              </ul>
+            </div>)
+          }
+          <div className='domain-description'>
+            <b>Description:</b>
+            <div className='domain-text-description'>
+              {ReactHtmlParser(this.props.data.description)}
+            </div>
+          </div>
         </div>
       </Modal>
     )
@@ -71,19 +59,4 @@ SingleDomain.propTypes = {
   onCancel: PropTypes.func
 }
 
-const mapStateToProps = state => {
-  return {
-
-  }
-}
-
-const mapDispatchToProps = dispatch => ({
-
-})
-
-const component = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(SingleDomain)
-
-export default component
+export default SingleDomain
