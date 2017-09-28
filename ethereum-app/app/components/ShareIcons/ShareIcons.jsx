37f8e055ce64e2408cd89b7d764caa29352740fa
Grimm <Andrey.Grimm.1998@gmail.com>
2017-09-28 19:54:24 +0300

    update ShareIcons and title

--- ethereum-app/app/components/ShareIcons/ShareIcons.jsx
+++ ethereum-app/app/components/ShareIcons/ShareIcons.jsx
@@ -6,7 +6,7 @@ import './ShareIcons.styl'
 class ShareIcons extends React.Component {
   static defaultProps = {
     url: location.origin,
-    title: 'Buy or Sell ENS .eth Domains. Blockchain Domains Market.'
+    title: 'Buy or Sell #ENS .eth Domains. Blockchain Domains Market.'
   }
   static propTypes = {
     url: PropTypes.string,
@@ -25,21 +25,19 @@ class ShareIcons extends React.Component {
     const url = 'https://plus.google.com/share?' + 'text=' + encodeURIComponent(this.props.title) + '&url=' + encodeURIComponent(this.props.url) + '&counturl=' + encodeURIComponent(this.props.url)
     window.open(url, '', 'toolbar=0,status=0,width=626,height=436')
   }
-  render () {
-    return (
-      <div className={'share-icons ' + (this.props.className ? this.props.className : '')}>
-        <Button onClick={this.facebook} className='icon-default icon-facebook icon-visible'>
-          <i className='fa fa-facebook' aria-hidden='true' />
-        </Button>
-        <Button onClick={this.twitter} className='icon-default icon-twitter icon-visible'>
-          <i className='fa fa-twitter' aria-hidden='true' />
-        </Button>
-        <Button onClick={this.googleplus} className='icon-default icon-google-plus icon-visible'>
-          <i className='fa fa-google-plus' aria-hidden='true' />
-        </Button>
-      </div>
-    )
-  }
+  render = () => (
+    <div className={'share-icons ' + (this.props.className ? this.props.className : '')}>
+      <Button onClick={this.facebook} className='icon-default icon-facebook icon-visible'>
+        <i className='fa fa-facebook' aria-hidden='true' />
+      </Button>
+      <Button onClick={this.twitter} className='icon-default icon-twitter icon-visible'>
+        <i className='fa fa-twitter' aria-hidden='true' />
+      </Button>
+      <Button onClick={this.googleplus} className='icon-default icon-google-plus icon-visible'>
+        <i className='fa fa-google-plus' aria-hidden='true' />
+      </Button>
+    </div>
+  )
 }
 
 export default ShareIcons
