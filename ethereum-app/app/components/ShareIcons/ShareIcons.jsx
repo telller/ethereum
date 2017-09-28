62de33566cfb17b6838cc49d93163f8e96b8b813
Grimm <Andrey.Grimm.1998@gmail.com>
2017-09-28 19:18:06 +0300

    update ShareIcons

--- ethereum-app/app/components/ShareIcons/ShareIcons.jsx
+++ ethereum-app/app/components/ShareIcons/ShareIcons.jsx
@@ -4,50 +4,42 @@ import PropTypes from 'prop-types'
 import './ShareIcons.styl'
 
 class ShareIcons extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      url: ''
-    }
+  static defaultProps = {
+    url: location.origin,
+    title: 'Buy or Sell ENS .eth Domains. Blockchain Domains Market.'
   }
-  facebook (purl, ptitle) {
-    this.state.url = ('http://www.facebook.com/sharer.php?s=100' + '&title=' + encodeURIComponent(ptitle) + '&u=' + encodeURIComponent(purl))
-    window.open(this.state.url, '', 'toolbar=0,status=0,width=626,height=436')
+  static propTypes = {
+    url: PropTypes.string,
+    title: PropTypes.string,
+    className: PropTypes.string
   }
-  twitter (purl, ptitle) {
-    this.state.url = 'http://twitter.com/share?' + 'text=' + encodeURIComponent(ptitle) + '&url=' + encodeURIComponent(purl) + '&counturl=' + encodeURIComponent(purl)
-    window.open(this.state.url, '', 'toolbar=0,status=0,width=626,height=436')
+  facebook = () => {
+    const url = ('http://www.facebook.com/sharer.php?s=100' + '&u=' + encodeURIComponent(this.props.url))
+    window.open(url, '', 'toolbar=0,status=0,width=626,height=436')
   }
-  googleplus (purl, ptitle) {
-    this.state.url = 'https://plus.google.com/share?' + 'text=' + encodeURIComponent(ptitle) + '&url=' + encodeURIComponent(purl) + '&counturl=' + encodeURIComponent(purl)
-    window.open(this.state.url, '', 'toolbar=0,status=0,width=626,height=436')
+  twitter = () => {
+    const url = 'http://twitter.com/share?' + 'text=' + encodeURIComponent(this.props.title) + '&url=' + encodeURIComponent(this.props.url) + '&counturl=' + encodeURIComponent(this.props.url)
+    window.open(url, '', 'toolbar=0,status=0,width=626,height=436')
+  }
+  googleplus = () => {
+    const url = 'https://plus.google.com/share?' + 'text=' + encodeURIComponent(this.props.title) + '&url=' + encodeURIComponent(this.props.url) + '&counturl=' + encodeURIComponent(this.props.url)
+    window.open(url, '', 'toolbar=0,status=0,width=626,height=436')
   }
   render () {
     return (
       <div className={'share-icons ' + (this.props.className ? this.props.className : '')}>
-        <Button onClick={() => this.facebook(this.props.url, this.props.title)} className={'icon-default icon-facebook ' + (this.state.showIcons ? 'icon-visible' : '')}>
+        <Button onClick={this.facebook} className='icon-default icon-facebook icon-visible'>
           <i className='fa fa-facebook' aria-hidden='true' />
         </Button>
-        <Button onClick={() => this.twitter(this.props.url, this.props.title)} className={'icon-default icon-twitter ' + (this.state.showIcons ? 'icon-visible' : '')}>
+        <Button onClick={this.twitter} className='icon-default icon-twitter icon-visible'>
           <i className='fa fa-twitter' aria-hidden='true' />
         </Button>
-        <Button onClick={() => this.googleplus(this.props.url, this.props.title)} className={'icon-default icon-google-plus ' + (this.state.showIcons ? 'icon-visible' : '')}>
+        <Button onClick={this.googleplus} className='icon-default icon-google-plus icon-visible'>
           <i className='fa fa-google-plus' aria-hidden='true' />
         </Button>
       </div>
     )
   }
-}
-
-ShareIcons.defaultProps = {
-  url: location.origin,
-  title: 'Buy or Sell ENS .eth Domains. Blockchain Domains Market.'
-}
-
-ShareIcons.propTypes = {
-  url: PropTypes.string,
-  title: PropTypes.string,
-  className: PropTypes.string
 }
 
 export default ShareIcons
